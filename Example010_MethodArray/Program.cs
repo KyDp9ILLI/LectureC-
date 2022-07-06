988820ac59fe2021c999b51ea5c6e69672e5affb
KyDp9ILLI <a-chikilev@mail.ru>
2022-07-06 19:48:13 +0300

    add ex_010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = {1,12,31,18,15,16,17,18,19};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index<n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+
+}
